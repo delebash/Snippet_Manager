ce4156c12e8047c05f5c67cb1c5094e6ada0214c
Dan Elebash <danelebash@hotmail.com>
2019-02-09 19:08:41 -0500

    Initial commit

---
+++ src/syncfusion.js
@@ -0,0 +1,12 @@
+import { Button } from '@syncfusion/ej2-buttons';
+
+
+export class Syncfusion {
+constructor(){
+
+}
+attached(){
+  let button = new Button();
+  button.appendTo('#normalbtn');
+}
+}
